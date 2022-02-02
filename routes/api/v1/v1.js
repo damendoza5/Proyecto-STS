0a6cc0bc112910145926bc4cb668a0f6b9654eee
damendoza5 <damendoza97@gmail.com>
2022-02-01 19:53:50 -0600

    1-2-2022

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { verifyApiHeaderToken } = require('./headerVerifyMiddelware')
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
-// const expedientesRoutes = require('./expedientes/expedientes');
-//middlewares
-router.use('/pacientes', pacientesRoutes);
-router.use('/expedientes', expedientesRoutes);
-// router.use('/expedientes', expedientesRoutes);
+
+router.use(
+    '/pacientes', 
+    verifyApiHeaderToken, 
+    pacientesRoutes
+);
+
+router.use(
+    '/expedientes', 
+    verifyApiHeaderToken, 
+    expedientesRoutes
+);
+
 module.exports = router;
